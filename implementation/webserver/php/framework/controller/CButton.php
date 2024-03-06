a5edace17635d3b9c49851a2e861e5174e7ec784
Daniel Simanek <simandan@cvut.cz>
2024-03-06 19:12:52 +0100

    Added argument functionality.

--- implementation/webserver/php/framework/controller/CButton.php
+++ implementation/webserver/php/framework/controller/CButton.php
@@ -2,15 +2,17 @@
 
 class CButton extends Controller {
     protected $method;
+    protected $argument;
 
-    public function __construct(MModel &$model, $formField, $method) {
+    public function __construct(MModel &$model, $formField, $method, $argument = null) {
         parent::__construct($model, $formField);
         $this->method = $method;
+        $this->argument = $argument;
     }
 
 
     public function update() {
         $m = $this->method;
-        if(isset($_POST[$this->formField])) $this->model->$m();
+        if(isset($_POST[$this->formField])) $this->model->$m($this->argument);
     }
 }
